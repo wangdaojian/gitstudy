825e43392e8f75ec19757b19be47c6346a354768
wangdaojian <wangdaojian@yazuo.com>
2018-11-16 10:52:45 +0800

    重新试下

--- src/main/java/com/daojian/HelloGit.java
+++ src/main/java/com/daojian/HelloGit.java
@@ -2,6 +2,7 @@ package com.daojian;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git, i'am from idea");
+        System.out.println("hello git, i'am from idea 你也测试下");
+
     }
 }
