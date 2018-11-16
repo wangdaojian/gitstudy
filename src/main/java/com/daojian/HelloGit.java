fe0ffa9906c31c43cc7b111b27e119a5eb5cc2d1
wangdaojian <wangdaojian@yazuo.com>
2018-11-16 11:27:04 +0800

    陈公公啦

--- src/main/java/com/daojian/HelloGit.java
+++ src/main/java/com/daojian/HelloGit.java
@@ -2,7 +2,7 @@ package com.daojian;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git, i'am from idea 你也测试下");
-
+        System.out.println("hello git, i'am from idea 你也测试下 ");
+        System.out.println("日了成功乐啊");
     }
 }
